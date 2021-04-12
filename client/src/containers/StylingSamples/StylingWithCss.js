351e9cc96f18532a92eecbab8f6c2869513cb89a
Kaisa Selamo <kaisa.selamo@gmail.com>
2021-04-12 22:17:13 +0300

    Päivä 4 Loppu

--- client/src/containers/StylingSamples/StylingWithCss.js
+++ client/src/containers/StylingSamples/StylingWithCss.js
@@ -5,14 +5,14 @@ import "./StylingWithCss.css";
 function StylingWithCss(props) {
   return (
     <form id="styling-with-css">
-      <div>
+      <div className="section-container">
         <div className="section">
           <h3>Billing Address</h3>
-          <div>
+          <div className="input-group">
             <label htmlFor="fullname">Full Name</label>
             <input type="text" name="fullname" placeholder="Maisa Mallikas" />
           </div>
-          <div>
+          <div className="input-group">
             <label htmlFor="email">Email</label>
             <input
               type="text"
@@ -20,34 +20,36 @@ function StylingWithCss(props) {
               placeholder="maisa.mallikas@example.com"
             />
           </div>
-          <div>
+          <div className="input-group">
             <label htmlFor="address">Address</label>
             <input type="text" name="address" placeholder="Länsikatu 15" />
           </div>
-          <div>
-            <label htmlFor="city">City</label>
-            <input type="text" name="city" placeholder="Joensuu" />
-          </div>
-          <div>
-            <label htmlFor="zip">Zip</label>
-            <input type="text" name="zip" placeholder="80110" />
+          <div className="section-container">
+            <div className="input-group section">
+              <label htmlFor="city">City</label>
+              <input type="text" name="city" placeholder="Joensuu" />
+            </div>
+            <div className="input-group section">
+              <label htmlFor="zip">Zip</label>
+              <input type="text" name="zip" placeholder="80110" />
+            </div>
           </div>
         </div>
 
         <div className="section">
           <h3>Payment</h3>
-          <div>
+          <div className="input-group">
             <label htmlFor="cardType">Select Card</label>
             <select name="cardType">
               <option value="visa">Visa</option>
               <option value="masterCard">Mastercard</option>
             </select>
           </div>
-          <div>
+          <div className="input-group">
             <label htmlFor="cardName">Name on Card</label>
             <input type="text" name="cardName" placeholder="Maisa Mallikas" />
           </div>
-          <div>
+          <div className="input-group">
             <label htmlFor="cardNumber">Credit card number</label>
             <input
               type="text"
@@ -55,16 +57,16 @@ function StylingWithCss(props) {
               placeholder="1111-2222-3333-4444"
             />
           </div>
-          <div>
-            <div>
+          <div className="section-container">
+            <div className="input-group section">
               <label htmlFor="cardExpMonth">Exp Month</label>
               <input type="number" name="cardExpMonth" placeholder="06" />
             </div>
-            <div>
+            <div className="input-group section">
               <label htmlFor="cardExpYear">Exp Year</label>
               <input type="number" name="cardExpYear" placeholder="2020" />
             </div>
-            <div>
+            <div className="input-group sevction">
               <label htmlFor="cardCvv">CVV</label>
               <input type="text" name="cardCvv" placeholder="123" />
             </div>
