9ca240f69ac529f17b15e2e1a9922fc68f56d552
Kaisa Selamo <kaisa.selamo@gmail.com>
2021-04-16 09:10:47 +0300

    Päivä 6 loppu

--- client/src/containers/Cars/Car.js
+++ client/src/containers/Cars/Car.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import { Col } from "reactstrap";
+import { Button, Col } from "reactstrap";
+import { FaTimes } from "react-icons/fa";
 
 export default function Car(props) {
-    const {make, model} =props;
+    const {make, model, onDelete} =props;
     return (
         <Col>
             <div make={make} className="car rounded-circle text-center">
                 {model}
             </div>
-      </Col>
+            <Button 
+                outline color="danger" 
+                className="car-button-delete"
+                onClick={() => onDelete()}
+            >
+                <FaTimes />
+            </Button>
+        </Col>
     );
 }
 
@@ -17,6 +25,7 @@ export default function Car(props) {
 Car.propTypes = {
     make: PropTypes.string.isRequired,
     model: PropTypes.string.isRequired,
+    onDelete: PropTypes.func,
 };
 
 //default propseja käytetään silloin, kun arvoa ei ole annettu
