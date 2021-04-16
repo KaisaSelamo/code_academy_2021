c5eecf2efadb321f8634c26ecafaaaa9bb0e6939
Kaisa Selamo <kaisa.selamo@gmail.com>
2021-04-16 11:19:56 +0300

    Päivä 7 Part 1 changes

--- client/src/containers/Cars/Cars.js
+++ client/src/containers/Cars/Cars.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-import React, {useState, useEffect} from "react";
-import { Row } from "reactstrap";
-import AddCar from "./AddCar.js";
-=======
 import React, { useEffect } from "react";
 import { Row } from "reactstrap";
 import { useSelector, useDispatch } from "react-redux";
 import AddCar from "./AddCar";
->>>>>>> 6b234f8... Task #8 part 1
 import Car from "./Car";
 import "./Cars.css";
 import { getCars } from "./carsSlice";
@@ -17,16 +11,8 @@ export default function Cars(props) {
   const dispatch = useDispatch();
 
   useEffect(() => {
-<<<<<<< HEAD
-    fetch("/api/cars")
-    .then((response) => response.json())
-    .then ((data) => setCars(data))
-    .catch((error) => console.log(error));
-  }, []);
-=======
     dispatch(getCars());
   }, [dispatch]);
->>>>>>> 6b234f8... Task #8 part 1
 
   const removeCar = (id) => {
     // fetch(`/api/cars/${id}`, {
@@ -46,19 +32,6 @@ export default function Cars(props) {
 
   return (
     <React.Fragment>
-<<<<<<< HEAD
-      <AddCar onCarAdded={(car) => setCars([...cars, car])} />
-        <Row id="cars">
-          {cars.map((car) => (
-            <Car 
-              key={car.id} 
-              make={car.make} 
-              model={car.model}
-              onDelete={() => removeCar(car.id)}
-              />
-          ))}
-        </Row>
-=======
       <AddCar onCarAdded={() => {} /*(car) => setCars([...cars, car])*/} />
       <Row id="cars">
         {carList.map((car) => (
@@ -70,7 +43,6 @@ export default function Cars(props) {
           />
         ))}
       </Row>
->>>>>>> 6b234f8... Task #8 part 1
     </React.Fragment>
   );
 
