2f4830690292ddf740a3af50fb89c07b8579dcc3
Antti Vikman <antti.vikman@nolwenture.com>
2021-04-16 10:41:51 +0300

    Päivä 7 Part 1 Merge

--- client/src/containers/Cars/Cars.js
+++ client/src/containers/Cars/Cars.js
@@ -1,28 +1,52 @@
+<<<<<<< HEAD
 import React, {useState, useEffect} from "react";
 import { Row } from "reactstrap";
 import AddCar from "./AddCar.js";
+=======
+import React, { useEffect } from "react";
+import { Row } from "reactstrap";
+import { useSelector, useDispatch } from "react-redux";
+import AddCar from "./AddCar";
+>>>>>>> 6b234f8... Task #8 part 1
 import Car from "./Car";
 import "./Cars.css";
+import { getCars } from "./carsSlice";
 
 export default function Cars(props) {
-  const [cars, setCars] = useState([]);
+  const { carList, loading, error } = useSelector((state) => state.cars);
+  const dispatch = useDispatch();
+
   useEffect(() => {
+<<<<<<< HEAD
     fetch("/api/cars")
     .then((response) => response.json())
     .then ((data) => setCars(data))
     .catch((error) => console.log(error));
   }, []);
+=======
+    dispatch(getCars());
+  }, [dispatch]);
+>>>>>>> 6b234f8... Task #8 part 1
 
   const removeCar = (id) => {
-    fetch(`/api/cars/${id}`, {
-      method: "DELETE",
-    }).then(() => {
-      setCars(cars.filter((car) => car.id !== id));
-    });
+    // fetch(`/api/cars/${id}`, {
+    //   method: "DELETE",
+    // }).then(() => {
+    //   setCars(cars.filter((car) => car.id !== id));
+    // });
   };
+
+  if (error) {
+    return error;
+  }
+
+  if (loading) {
+    return "Loading...";
+  }
 
   return (
     <React.Fragment>
+<<<<<<< HEAD
       <AddCar onCarAdded={(car) => setCars([...cars, car])} />
         <Row id="cars">
           {cars.map((car) => (
@@ -34,6 +58,19 @@ export default function Cars(props) {
               />
           ))}
         </Row>
+=======
+      <AddCar onCarAdded={() => {} /*(car) => setCars([...cars, car])*/} />
+      <Row id="cars">
+        {carList.map((car) => (
+          <Car
+            key={car.id}
+            make={car.make}
+            model={car.model}
+            onDelete={() => removeCar(car.id)}
+          />
+        ))}
+      </Row>
+>>>>>>> 6b234f8... Task #8 part 1
     </React.Fragment>
   );
 
