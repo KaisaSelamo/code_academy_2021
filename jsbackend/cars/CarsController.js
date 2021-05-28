b251144c2f1a16c2374538ecbcbc93ec87cba9fc
Antti Vikman <antti.vikman@nolwenture.com>
2021-05-28 08:12:24 +0300

    Päivä 16 Alku

---
+++ jsbackend/cars/CarsController.js
@@ -0,0 +1,11 @@
+const cars = 
+
+export default (app) => {
+    app.get("/Cars", (req, res) => {
+        res.json(cars);
+    });
+
+app.post("/Cars", req, res) => {
+    
+}
+}
--- jsbackend/index.js
+++ jsbackend/index.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  const {
+    headers: { cookie },
+    params,
+    query,
+  } = req;
+  console.log({ cookie, params, query });
+  res.setHeader("Set-Cookie", "trackerId=tosi_salainen_koodi");
+  res.send("Hello World!");
+});
+
+
+app.get("/Chunks", (req, res) => {
+    res.header("transfer-encoding", "chunked");
+    res.set("Content-Type", "text/json");
+    const interval = setInterval(() => res.write("A"), 1000);
+    setTimeout(() => {
+      clearInterval(interval);
+      res.end();
+    }, 10000);
+  });
+
+app.get("/Fruits", (req, res) => {
+  const fruits = [{ type: "apple", name: "Golden" }];
+  res.json(fruits);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
